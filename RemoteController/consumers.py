f3ed678594d025c4a7d999a499c518554c909619
iiitsyogesh <yogeshkaushikpersonal@gmail.com>
2022-03-06 18:07:45 +0530

    needed secrets added for heroku

--- RemoteController/consumers.py
+++ RemoteController/consumers.py
@@ -7,7 +7,15 @@ from asgiref.sync import async_to_sync
 channel_layer = get_channel_layer()
 
 import pyrebase
-from .firebase_credentials import firebaseConfig
+firebaseConfig = {
+    'apiKey': "AIzaSyD0TftQQlr--6pSMyUXzw5QT4pFM1kB-HM",
+    'authDomain': "smart-home-16c6c.firebaseapp.com",
+    'databaseURL': "https://smart-home-16c6c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    'projectId': "smart-home-16c6c",
+    'storageBucket': "smart-home-16c6c.appspot.com",
+    'messagingSenderId': "1020150526141",
+    'appId': "1:1020150526141:web:d0ef2d2dd41178838447d4"
+}
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
--- RemoteController/views.py
+++ RemoteController/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import View
-from .firebase_credentials import firebaseConfig
 import pyrebase
 
+firebaseConfig = {
+    'apiKey': "AIzaSyD0TftQQlr--6pSMyUXzw5QT4pFM1kB-HM",
+    'authDomain': "smart-home-16c6c.firebaseapp.com",
+    'databaseURL': "https://smart-home-16c6c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    'projectId': "smart-home-16c6c",
+    'storageBucket': "smart-home-16c6c.appspot.com",
+    'messagingSenderId': "1020150526141",
+    'appId': "1:1020150526141:web:d0ef2d2dd41178838447d4"
+}
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 db = firebase.database()
